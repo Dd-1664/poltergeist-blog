dcfbf8734ddeae5bc531df2e39485e42adb4b022
Dd-1664 <david.dabo.pro@gmail.com>
2018-05-03 14:28:29 +0200

    create loop to display article

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,14 +4,30 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Article;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('AppBundle:Default:default.html.twig');
+        $article = $this->getDoctrine()
+        ->getRepository(Article::class)
+        ->findAll();
+
+
+
+
+
+
+        return $this->render('AppBundle:Default:default.html.twig',
+            [
+                'article' => $article
+                // 'form' => $form->createView()
+            ]
+        );
     }
 }
